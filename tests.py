7f7fb4b199db3e709136b65d04e72b857689bf8c
tonyjward <tony@statcore.co.uk>
2020-02-24 17:01:11 +0000

    Update tests

--- tests.py
+++ tests.py
@@ -1,7 +1,13 @@
 from twlearn import MyLinearRegression, generate_dataset
+from twlearn.metrics import Rmse, Mae
 
 if __name__ == '__main__':
     X_train, y_train, X_test, y_test = generate_dataset(10,10,2)
     model = MyLinearRegression()
     model.fit(X_train, y_train)
-    model.coef()
+    model.coefficients()
+    predictions = model.predict(X_test)
+    RMSE = Rmse(predictions, y_test)
+    MAE = Mae(predictions, y_test)
+    print(f"RMSE: {RMSE}")
+    print(f"MAE: {MAE}")
