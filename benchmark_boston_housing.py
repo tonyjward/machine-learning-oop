d11ac5e04a186861c132d3069e2e79eb4e90cc6b
tonyjward <tony@statcore.co.uk>
2020-03-06 15:53:51 +0000

    Incorrectly compare differences using t test on 10-fold cross validation

--- benchmark_boston_housing.py
+++ benchmark_boston_housing.py
@@ -24,10 +24,16 @@ if __name__ == '__main__':
     X = np.array(boston.data)
     Y = np.array(boston.target)
 
-    NO_FOLDS = 50
-    folds = np.random.randint(low = 0, high = NO_FOLDS , size = len(Y))
+    no_examples, no_features = X.shape
 
+    # LOOCV
+    # NO_FOLDS = no_examples
+    # folds = np.arange(no_examples)
 
+    # 10-fold
+    NO_FOLDS = 10
+    np.random.seed(2020)
+    folds = np.random.randint(low = 0, high = NO_FOLDS , size = no_examples)
 
     OLS_MAE = np.zeros(NO_FOLDS)
     OLS_RMSE = np.zeros(NO_FOLDS)
@@ -48,7 +54,7 @@ if __name__ == '__main__':
 
         # train models
         ols_model.fit(X_train, Y_train, optimiser = 'OLS')
-        pso_model.fit(X_train, Y_train, optimiser = 'PSO', loss = Mae_pso, num_iterations = 500)
+        pso_model.fit(X_train, Y_train, optimiser = 'PSO', loss = Mae_pso, num_iterations = 500, no_particles = 300)
 
         # evaluate models
         OLS_MAE[fold] = evaluate(ols_model, X_test, Y_test, Mae)
@@ -56,14 +62,25 @@ if __name__ == '__main__':
         OLS_RMSE[fold] = evaluate(ols_model, X_test, Y_test, Rmse)
         PSO_RMSE[fold] = evaluate(pso_model, X_test, Y_test, Rmse)
 
-    print(f"OLS MAE {np.mean(OLS_MAE)}")
-    print(f"OLD MAE Confidence intervale ({np.mean(OLS_MAE) - 1.96 * np.std(OLS_MAE)} , {np.mean(OLS_MAE) + 1.96 * np.std(OLS_MAE)}")
-    print(f"PSO MAE {np.mean(PSO_MAE)}")
-    print(f"OLD MAE Confidence intervale ({np.mean(PSO_MAE) - 1.96 * np.std(PSO_MAE)} , {np.mean(PSO_MAE) + 1.96 * np.std(PSO_MAE)}")
-    print(f"OLS RMSE {np.mean(OLS_RMSE)}")
-    print(f"PSO RMSE {np.mean(PSO_RMSE)}")
+    MAE_diff = OLS_MAE - PSO_MAE
+    RMSE_diff = OLS_RMSE- PSO_RMSE
 
-                
+    def sample_std(x):
+        return np.sqrt(np.sum(((x - np.mean(x))**2))/(len(x)-1))
+
+    t_statistic = 2.262
+
+    print(f"OLS MAE Average {np.mean(OLS_MAE)}")
+    print(f"OLD MAE Confidence interval ({np.mean(OLS_MAE) - t_statistic * sample_std(OLS_MAE)/(NO_FOLDS -1)} , {np.mean(OLS_MAE) + t_statistic * sample_std(OLS_MAE)/(NO_FOLDS -1)}")
+    print(f"PSO MAE Average {np.mean(PSO_MAE)}")
+    print(f"OLD MAE Confidence interval ({np.mean(PSO_MAE) - t_statistic * sample_std(PSO_MAE)/(NO_FOLDS -1)} , {np.mean(PSO_MAE) + t_statistic * sample_std(PSO_MAE)/(NO_FOLDS -1)}")
+    print(f"OLS - PSO: MAE Average {np.mean(MAE_diff)}")
+    print(f"OLD - PSO: MAE Confidence interval ({np.mean(MAE_diff) - t_statistic * sample_std(MAE_diff)/(NO_FOLDS -1)} , {np.mean(MAE_diff) + t_statistic * sample_std(MAE_diff)/(NO_FOLDS -1)}")
+    print(f"OLS - PSO: RMSE Average {np.mean(RMSE_diff)}")
+    print(f"OLD - PSO: RMSE Confidence interval ({np.mean(RMSE_diff) - t_statistic * sample_std(RMSE_diff)/(NO_FOLDS -1)} , {np.mean(RMSE_diff) + t_statistic * sample_std(RMSE_diff)/(NO_FOLDS -1)}")
+
+    print(f"OLS RMSE Average {np.mean(OLS_RMSE)}")
+    print(f"PSO RMSE Average {np.mean(PSO_RMSE)}")            
 
 
         
