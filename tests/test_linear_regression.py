37f2389489a5c9f9e0d7d4cb9cbe2c6770b920fb
tonyjward <tony@statcore.co.uk>
2020-03-09 14:57:45 +0000

    Refactor test suite using test discover

--- tests/test_linear_regression.py
+++ tests/test_linear_regression.py
@@ -80,23 +80,6 @@ class Test_LinearRegression_ParticleSwarm_MAE_1D_Class(unittest.TestCase):
         yhat = self.lm.predict(X_new)
         self.assertIsNone(np.testing.assert_allclose(desired_yhat, yhat))
 
-class Test_Five_By_Two_Cv(unittest.TestCase):
-    def setUp(self):
-        self.errorA = {0:{0:5., 1:4.},
-                       1:{0:4., 1:3.},
-                       2:{0:3., 1:3.},
-                       3:{0:2., 1:3.},
-                       4:{0:1., 1:3.}}
-        self.errorB = {0:{0:7., 1:8.},
-                       1:{0:6., 1:5.},
-                       2:{0:3., 1:9.},
-                       3:{0:6., 1:10.},
-                       4:{0:8., 1:7.}}
-    
-    def test_five_by_two_cv(self):
-        t_statistic, average_differences = five_by_two_cv(self.errorA, self.errorB)
-        self.assertIsNone(np.testing.assert_allclose(t_statistic, -0.830454799))
-
 # class TestLinearRegressionGradientDescent_MAE_1D_Class(unittest.TestCase):
     
 #     def setUp(self):
